a529a863c5fc59246409f16c9d1f5a1c602652ef
tcp3256 <tcp3256@cs.rit.edu>
2017-03-17 12:58:10 -0400

    Initial commit

---
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.firstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.example.firstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    /**
+     * Called when the user clicks the send button
+     */
+    public void sendMessage(View view){
+        //Do something in response to the button click
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
